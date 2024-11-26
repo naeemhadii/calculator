a78a00663a2c1fb5544577ccb186efbea1439098
Muhammad Naeem <naeemhadi8149@gmail.com>
2024-11-26 21:54:04 +0500

    some code is write in it

--- myapp/main.py
+++ myapp/main.py
@@ -1,8 +1,29 @@
-import flet as ft
+from flet import*
 
+# add a class of calculator
 
-def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+class Calculator(UserControl):
+    def __init__(self,page,primary_color,secondary_color):
+        super().__init__()
+        self.page = page
+        self.primary_color = primary_color
+        self.secondary_color = secondary_color
+        self.build()
+    def build(self):
+        front_page = View(
+            route='/front_page',
+            controls=[
+                Text(
+                    value='hello world'
+                )
+            ]
+        )
+        self.page.views.append(front_page)
 
-
-ft.app(main)
+def main(page:Page):
+    primary_color = '#25D366'
+    secondary_color = '#075E54'
+    cal = Calculator(page,primary_color,secondary_color)
+    page.go('/front_page')
+if __name__ == '__main__':
+    app(target=main)
